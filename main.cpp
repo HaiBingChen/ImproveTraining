40aebd38afcd0b4444c1d48bc549ca50931c479f
chenhaibing <chenhaibing@cviauto.com>
2021-09-21 08:50:58 +0800

    [feature][CVIAUTO][JIRA#NA]First add some project files
    [what]首次添加上传工程文件和目录
    [why]软件技能提升
    [how]首次添加上传工程文件和目录

---
+++ main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by mike on 2021/9/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char** argv)
+{
+    std::cout << "This is the improve training for some of software skills" << std::endl;
+    return 0;
+}
